a3f9376ebf1cf9fa979b6ea70349630b133a7935
Eric Christine <ericjchristine@gmail.com>
2020-08-16 21:17:27 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -57,6 +57,7 @@ def add_characters
       :additional_characters => [
         {name: "Prince Escalus"},
         {name: "Apothecary"}
+        ]
    }
 
    # Write code below that does the following programmatically:
